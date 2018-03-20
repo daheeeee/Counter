96c0655d83aad79e517e028c6fb9b703b187e59b
D7703_11 <d7703_11@d7703-11ui-imac.local>
2018-03-20 15:37:58 +0900

    add second

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -7,14 +7,27 @@
 //
 
 import UIKit
+var count = 0
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    @IBOutlet weak var lab: UILabel!
+    
+    @IBAction func ButTap(_ sender: Any) {
+        count = count + 1
+        lab.text = "\(count)"
+        
+    }
+    @IBAction func butRe(_ sender: Any) {
+        
+        count = 0
+        
+        lab.text = "\(count)"
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
